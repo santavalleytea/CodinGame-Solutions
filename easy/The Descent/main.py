279e93d453b038ff5c67ed8091ce0ebf54c2afba
Takaya <119724608+santavalleytea@users.noreply.github.com>
2026-02-15 19:18:56 +1100

    Update main.py

---
+++ easy/The Descent/main.py
@@ -0,0 +1,23 @@
+import sys
+import math
+
+# The while loop represents the game.
+# Each iteration represents a turn of the game
+# where you are given inputs (the heights of the mountains)
+# and where you have to print an output (the index of the mountain to fire on)
+# The inputs you are given are automatically updated according to your last actions.
+
+
+# game loop
+while True:
+    max_height = 0
+    max_index = 0
+    for i in range(8):
+        mountain_h = int(input())  # represents the height of one mountain.
+        if max_height < mountain_h:
+            max_height = mountain_h
+            max_index = i
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)        
+    print(max_index)
